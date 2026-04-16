74ee44a0ec6d156e44f64ff1d0231e29f9d57339
teharada <tetsuya.harada@oracle.com>
2026-04-16 17:25:39 +0900

    update: 機能修正

--- lab_4-1/outputs.tf
+++ lab_4-1/outputs.tf
@@ -1,23 +1,24 @@
-output "vcn1_id" {
-  value = oci_core_vcn.vcn1.id
-}
-
-output "vcn2_id" {
-  value = oci_core_vcn.vcn2.id
-}
-
 output "vm1_id" {
   value = oci_core_instance.vm1.id
+}
+
+output "vm1_private_ip" {
+  value = oci_core_instance.vm1.private_ip
+}
+
+output "vm1_public_ip" {
+  value = oci_core_instance.vm1.public_ip
 }
 
 output "vm2_id" {
   value = oci_core_instance.vm2.id
 }
 
-output "vcn1_lpg_id" {
-  value = oci_core_local_peering_gateway.vcn1_lpg.id
+output "vm2_private_ip" {
+  value = oci_core_instance.vm2.private_ip
 }
 
-output "vcn2_lpg_id" {
-  value = oci_core_local_peering_gateway.vcn2_lpg.id
+output "vm2_public_ip" {
+  value = oci_core_instance.vm2.public_ip
 }
+
